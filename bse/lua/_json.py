131aae9b9e4f40f4d30318e25eb7709d69bbf269
Germán Fuentes Capella <german@fuentescapella.com>
2020-04-03 11:50:27 +0200

    completing support for json

--- bse/lua/_json.py
+++ bse/lua/_json.py
@@ -2,15 +2,36 @@
 
 import attr
 import json
+from bse import logger
 from typing import Any, Dict, Optional
 
 
 @attr.s
 class JSON(object):
 
-    _json: Optional[str] = attr.ib(default="")
+    _jsontext: Optional[str] = attr.ib(default="")
+    _json: Optional[Any] = attr.ib(default=None)
+    _log: logger.Logger = attr.ib()
+
+    @_log.default
+    def _initlog(self) -> logger.Logger:
+        return logger.new(self.__class__.__name__)
 
     def dictionary(self) -> Dict[str, Any]:
-        if not self._json:
-            self._json = "{}"
-        return json.loads(self._json)
+        self._log.debug(f"[JSON(...):dictionary()] {self._jsontext}")
+        if self._json:
+            return self._json
+        if not self._jsontext:
+            self._jsontext = "{}"
+        self._json = json.loads(self._jsontext)
+        return self._json
+
+    def set(self, j: Any) -> "JSON":
+        self._json = j
+        self._jsontext = json.dumps(j)
+        self._log.debug(f"[JSON():set(...)] {self._jsontext}")
+        return self
+
+    def json(self) -> str:
+        self._log.debug(f"[JSON():json()] {self._jsontext}")
+        return self._jsontext or ""
--- tests/luatests/test_json.lua
+++ tests/luatests/test_json.lua
@@ -4,3 +4,7 @@ local jsonText = '{"key": "value"}'
 local json_parser = JSON(jsonText)
 local json = json_parser:dictionary()
 assert(json["key"] == "value")
+
+json["key"] = "another value"
+jsonText = json_parser:set(json):json()
+assert(jsonText == '{"key": "another value"}')
