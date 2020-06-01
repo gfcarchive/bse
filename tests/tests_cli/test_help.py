ba2270f8b4cdf2e34855a4f5a7413b6bdb090118
Germán Fuentes Capella <german@fuentescapella.com>
2020-06-01 11:31:17 +0200

    parametrizing test for checking help messages

--- tests/tests_cli/test_help.py
+++ tests/tests_cli/test_help.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import mockup_help as mh
+import pytest  # type: ignore
 from bse import cli
 from click.testing import CliRunner
 
 
-def test_help() -> None:
-    commands = list(cli.main.commands.keys())
-    for command in commands:
-        runner = CliRunner()
-        help_result = runner.invoke(cli.main, [command, "--help"])
-        assert help_result.exit_code == 0
-        assert help_result.output == mh.help[command]
+@pytest.mark.parametrize("command", list(cli.main.commands.keys()))
+def test_help(command) -> None:
+    runner = CliRunner()
+    help_result = runner.invoke(cli.main, [command, "--help"])
+    assert help_result.exit_code == 0
+    assert help_result.output == mh.help[command]
