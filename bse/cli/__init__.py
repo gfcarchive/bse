f54945d11a32ebc3d6e24e1f1a983dd0c6ce2060
Germán Fuentes Capella <german@fuentescapella.com>
2020-06-01 12:35:27 +0200

    merging all commands related to the engine methods, as they were mostly similar

--- bse/cli/__init__.py
+++ bse/cli/__init__.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from ._accounts import cmd_accounts
-from ._balance import cmd_balance
+from ._engine import cmd_accounts, account_detail_cmds
 from ._config import cmd_config
 from ._main import main
-from ._securities import cmd_securities
 from ._slugs import cmd_slugs
-from ._transfers import cmd_transfers
 
 main.add_command(cmd_accounts)
-main.add_command(cmd_balance)
 main.add_command(cmd_config)
-main.add_command(cmd_securities)
 main.add_command(cmd_slugs)
-main.add_command(cmd_transfers)
+
+for cmd in account_detail_cmds:
+    main.add_command(cmd)
---
+++ bse/cli/_engine.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import click
+from bse import config, logger
+from bse.engine import Engine
+from bse.transform import Jsonable
+from typing import Any, Callable, Dict
+
+
+@click.command(name="accounts")
+@click.argument("slug", type=click.STRING)
+@click.pass_obj
+@logger.logexceptions
+def cmd_accounts(obj: Dict[str, str], slug: str) -> int:
+    """Retrieves the list of accounts for a given slug."""
+    conf = config.new(obj)
+    with Engine(slug, conf) as e:
+        click.echo(Jsonable.dump(e.accounts()))
+    return 0
+
+
+_cmddef = (
+    ("balance", "Retrieves the balance in multiple currencies for a given slug."),
+    ("securities", "Retrieves the list of securities for a given slug."),
+    ("transfers", "Retrieves the list of transfers for a given slug."),
+)
+
+account_detail_cmds: Any = []
+
+
+def _newcmd(cmd: str, doc: str) -> Callable[..., Any]:
+    @click.command(name=cmd)
+    @click.argument("slug", type=click.STRING)
+    @click.pass_obj
+    @logger.logexceptions
+    def cmdf(obj: Dict[str, str], slug: str) -> int:
+
+        conf = config.new(obj)
+        with Engine(slug, conf) as e:
+            ret = []
+            for account in e.accounts():
+                f = getattr(e, cmd)
+                ret.append(f(account))
+            click.echo(Jsonable.dump(ret))
+        return 0
+
+    cmdf.help = doc
+    return cmdf
+
+
+for (cmd, doc) in _cmddef:
+    account_detail_cmds.append(_newcmd(cmd, doc))
